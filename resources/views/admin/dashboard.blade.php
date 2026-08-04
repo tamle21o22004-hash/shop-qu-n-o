62cb13bd6a539a8767ae0ce98c1833985183e12d
Thanh Tâm <tamle21o22004@gmail.com>
2026-08-04 10:51:58 +0700

    A

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
+   <span/>
     <script>
+
         tailwind.config = {
             theme: {
                 extend: {
